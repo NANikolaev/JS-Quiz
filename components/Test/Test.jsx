b3d6ac17f9beabd7f823c7dafa05aad8b01e73df
NANikolaev <91592401+NANikolaev@users.noreply.github.com>
2023-02-11 20:23:55 +0200

    Update Test.jsx

--- components/Test/Test.jsx
+++ components/Test/Test.jsx
@@ -1,4 +1,4 @@
-import { useState,useEffect} from "react";
+import { useState} from "react";
 import { AnimatePresence } from "framer-motion";
 import { StyledSection, StyledSubHeader } from "./elements";
 
